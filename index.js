5c01e814a14cffca2c01773256957ff8f37a5f47
zeke <zeke@sikelianos.com>
2014-07-30 23:28:25 -0700

    it works

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 
 require("dotenv").load()
 
+var fs = require("fs")
+var superagent = require("superagent")
 var flickr = require("flickr-client")({key: process.env.FLICKR_KEY})
 flickr.info = require("flickr-photo-info")(flickr)
 flickr.urls = require("flickr-generate-urls")
 
 flickr("photosets.getPhotos", {photoset_id: "72157610714160553"}, function (err, response) {
   if (err) throw err;
-
   response.photoset.photo.slice(0,100000).forEach(function(photo) {
-    // photo.format = "jpg"
-    // photo.urls = urls(photo)
     flickr.info(photo.id, function(err, i) {
       if (err) throw err;
-      console.log(i.urls.original);
+      var url = i.urls.original
+      console.log(url)
+      var filename = url.substring(url.lastIndexOf('/')+1)
+      superagent
+        .get(url)
+        .pipe(fs.createWriteStream(__dirname + "/downloads/" + filename))
     })
-    // console.log(photo)
   })
-
 })
